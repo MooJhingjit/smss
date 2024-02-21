cceaf74d96856925b2804d2a5dccb950697b8482
pokkrong-webquest <pokkrong@webquest.io>
2024-02-21 11:04:42 +0700

    login draft

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,43 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import LOGO from "@/public/logo.png";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-sm space-y-10">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
+
         <form className="space-y-6" action="#" method="POST">
-          <div className="relative -space-y-px rounded-md shadow-sm">
-            <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
+          <div className="relative space-y-2  rounded-md shadow-sm">
             <div>
-              <Label htmlFor="email-address" className="sr-only">
-                Email address
+              <Label htmlFor="name" className="sr-only">
+                ชื่อผู้ใช้
               </Label>
               <Input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="name"
+                name="name"
                 required
-                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-                placeholder="Email address"
+                placeholder="ชื่อผู้ใช้"
               />
             </div>
             <div>
               <Label htmlFor="password" className="sr-only">
-                Password
+                รหัสผ่าน
               </Label>
               <Input
                 id="password"
                 name="password"
                 type="password"
-                autoComplete="current-password"
                 required
-                className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-                placeholder="Password"
+                placeholder="รหัสผ่าน"
               />
             </div>
           </div>
@@ -54,26 +47,31 @@ export default function SignInPage() {
                 htmlFor="remember-me"
                 className="ml-3 block text-sm leading-6 text-gray-900"
               >
-                Remember me
+                จำฉันไว้
               </Label>
             </div>
 
             <div className="text-sm leading-6">
-              <a
-                href="#"
-                className="font-semibold text-primary-600 hover:text-primary-500"
-              >
-                Forgot password?
-              </a>
+              <p className="font-semibold text-primary-600 hover:text-primary-500">
+                ลืมรหัสผ่าน?
+              </p>
             </div>
           </div>
 
           <div>
             <Button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              className="flex w-full space-x-2 justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
-              Sign in
+              <span>
+                เข้าสู่ระบบ
+
+              </span>
+              <img
+                className="h-8 w-auto"
+                src={LOGO.src}
+                alt="Workflow"
+              />
             </Button>
           </div>
         </form>
